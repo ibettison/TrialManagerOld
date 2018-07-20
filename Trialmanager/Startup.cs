af7c9a652739f7a130461915506c1e560838e966
Ian Bettison <ian.bettison@ncl.ac.uk>
2018-07-20 09:21:18 +0100

    Add project files.

---
+++ Trialmanager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Trialmanager.Startup))]
+namespace Trialmanager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
